27c260c18072283787242d1e3a6ddd4109ee9e32
David Alcalde <david.alcalde.cu@gmail.com>
2026-05-12 12:50:01 -0500

    remove dio dependency from fz_api_handling

--- packages/fz_api_handling/lib/src/api_provider.dart
+++ packages/fz_api_handling/lib/src/api_provider.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_font_icons/flutter_font_icons.dart';
